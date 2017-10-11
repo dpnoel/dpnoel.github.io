5cd92a4f32ef4e30d4f6a6e8a854923d12c8f85c
dpnoel <shade8547@gmail.com>
2017-10-11 09:31:19 -0500

    test touch 2

--- touch/sketch.js
+++ touch/sketch.js
@@ -4,7 +4,6 @@ document.ontouchmove = function(event){
 
 function setup() {
  createCanvas(windowWidth,windowHeight);
- rectMode(CENTER);
  stroke(0);
  noFill();
  strokeWeight(5);
@@ -14,10 +13,9 @@ function draw(){
  background(255);
  
  for(var i=0;i<touches.length;i++){
-  rect(touches[i].x,touches[i].y,150,150);
+  ellipse(touches[i].x,touches[i].y,150,150);
   }
   beginShape(POINTS);
-  vertex(touches[1], touches[2]);
-  vertex(touches[2], touches[3]);
-  vertex(touches[3], touches[4]);
+  vertex(touches[i].x, touches[i].y);
+  endShape(CLOSE);
 }
