15279438895404537eb0e5090eda3fd655d55188
dpnoel <shade8547@gmail.com>
2017-10-11 09:33:18 -0500

    touch test 3

--- touch/sketch.js
+++ touch/sketch.js
@@ -12,10 +12,12 @@ function setup() {
 function draw(){
  background(255);
  
+ beginShape();
+  vertex(touches[i].x, touches[i].y);
+  endShape(CLOSE);
+  
  for(var i=0;i<touches.length;i++){
   ellipse(touches[i].x,touches[i].y,150,150);
   }
-  beginShape(POINTS);
-  vertex(touches[i].x, touches[i].y);
-  endShape(CLOSE);
+  
 }
