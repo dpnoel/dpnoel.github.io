c33663abddc5065a0f87b91f14f287ab336e5a63
dpnoel <shade8547@gmail.com>
2017-10-18 10:03:55 -0500

    billes2

--- billes/sketch.js
+++ billes/sketch.js
@@ -1,10 +1,31 @@
+var x = 0;
+var y = 0; 
+
+var Vx = 0;
+var Vy = 0;
+
+var Ax = 0;
+var Ay = 0;
+
+var backgroundColor(random(0,255),random(0,255),random(0,255));
+
+var vMulti = 0.01;
+var bMulti = 0.5;
+
 function setup() {
- createCanvas(windowWidth,windowHeight); 
+ createCanvas(windowWidth,windowHeight);
+ drawBall();
+ moveBall();
+ 
 }
 
 function draw() {
- background(255);
  textSize(40);
+ ellipse(50,50);
  text("Rx: " + rotationX, 100, 100);
  text("Ry: " + rotationY, 100, 150);
+}
+
+function deviceShaken(){
+  background(backgroundColor);
 }
