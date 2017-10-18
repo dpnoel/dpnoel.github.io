c4f3a5a8594439da1489c2bb438286f8d67c070a
dpnoel <shade8547@gmail.com>
2017-10-18 08:41:43 -0500

    bille test

---
+++ billes/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+ createCanvas(windowWidth,windowHeight); 
+}
+
+function draw() {
+ background(255);
+ textSize(40);
+ text("Rx: " + rotationX, 100, 100);
+ text("Ry: " + rotationY, 100, 150);
+}
