d9f08a52a8bba553367dbcff0af05542641b1ed1
dpnoel <shade8547@gmail.com>
2017-10-18 10:10:10 -0500

    billes4

--- billes/sketch.js
+++ billes/sketch.js
@@ -7,15 +7,12 @@ var Vy = 0;
 var Ax = 0;
 var Ay = 0;
 
-var backgroundColor = (random(55,255),random(55,255),random(55,255));
-
 var vMulti = 0.01;
 var bMulti = 0.5;
 
 function setup() {
  createCanvas(windowWidth,windowHeight);
- drawBall();
- moveBall();
+ var backgroundColor = (random(55,255),random(55,255),random(55,255));
  
 }
 
@@ -24,6 +21,8 @@ function draw() {
  ellipse(50,50);
  text("Rx: " + rotationX, 100, 100);
  text("Ry: " + rotationY, 100, 150);
+ /*drawBall();
+ moveBall();*/
 }
 
 function deviceShaken(){
